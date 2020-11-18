75d09945bd1b2a53be44276029820c44be45e630
Dilan Rey <dilanerey06@gmail.com>
2020-11-18 15:58:46 -0500

    Solucion final punto2 en dev-c++

--- quiz-punto2.cpp
+++ quiz-punto2.cpp
@@ -3,52 +3,95 @@
 #include<fstream>
 using namespace std;
 
-void readBin(float **ptrMatrix, int n);
-void imp(float **ptrMatrix, int n);
-void rect(float **ptrMatrix, int n);
+void readBin();
+void imp();
+void rectDerecha(int ite);
+void rectArriba(int ite);
+void rectAbajo(int ite);
+void rectDiag(int ite);
+float sum_func;
+float max_der=0, max_arr=0, max_aba=0, max_diag=0;
+float maxim=0;
+float **ptrMatrix; 
+int n=20;
 
 int main(){
-	float **ptrMatrix;
-	int n=20;
 	ptrMatrix=new float*[n];
 	for(int i=0; i<n; ++i){
 		ptrMatrix[i]=new float[n];
 	}
-	readBin(ptrMatrix, n);
-	imp(ptrMatrix, n);
-	rect(ptrMatrix, n);
+	readBin();
+	imp();
+	rectDerecha(0);
+	//cout<<"max_der= "<<max_der<<'\n';
+	rectArriba(0);
+	//cout<<"max_arr= "<<max_arr<<'\n';
+	rectAbajo(0);
+	//cout<<"max_aba= "<<max_aba<<'\n';
+	rectDiag(0);
+	//cout<<"max_diag= "<<max_aba<<'\n';
+	cout<<"Maxima suma: "<<maxim<<'\n';
 	delete[]ptrMatrix;
 	return 0;
 }
 
-void rect(float **ptrMatrix, int n){
-	float sum[4], max_sum=0;
-	for(int i=0; i<n; ++i){
-		for(int j=0; j<n; ++i){
-			sum[4]={};
-			if(i<17){
-				sum[0]=ptrMatrix[i][j]+ptrMatrix[i+1][j]+ptrMatrix[i+2][j]+ptrMatrix[i+3][j];
-			}
-			if(j<17){
-				sum[1]=ptrMatrix[i][j]+ptrMatrix[i][j+1]+ptrMatrix[i][j+2]+ptrMatrix[i][j+3];
-			}
-			if(j<17&&i<17){
-				sum[2]=ptrMatrix[i][j]+ptrMatrix[i+1][j+1]+ptrMatrix[i+2][j+2]+ptrMatrix[i+3][j+3];
-			}
-			if (i>2){
-				sum[3]=ptrMatrix[i][j]+ptrMatrix[i-1][j]+ptrMatrix[i-2][j]+ptrMatrix[i-3][j];
-			}
-			for(int k=0; k<4; ++k){
-				if(sum[k]>max_sum){
-					max_sum=sum[k];
-				}
-			}
+void rectDiag(int ite){
+	sum_func=0;
+	for(int i=0; i<17; ++i){
+		sum_func=ptrMatrix[ite][i]+ptrMatrix[ite+1][i+1]+ptrMatrix[ite+2][i+2]+ptrMatrix[ite+3][i+3];
+		if(sum_func>maxim){
+			maxim=sum_func;
 		}
 	}
-	cout<<"Suma maxima: "<<max_sum<<'\n';
+	if(ite<=15){
+		++ite;
+		rectDiag(ite);
+	}
 }
 
-void readBin(float **ptrMatrix, int n){
+void rectAbajo(int ite){
+	sum_func=0;
+	for(int i=0; i<17; ++i){
+		sum_func=ptrMatrix[i][ite]+ptrMatrix[i+1][ite]+ptrMatrix[i+2][ite]+ptrMatrix[i+3][ite];
+		if(sum_func>maxim){
+			maxim=sum_func;
+		}
+	}
+	if(ite<=18){
+		++ite;
+		rectArriba(ite);
+	}
+}
+
+void rectArriba(int ite){
+	sum_func=0;
+	for(int i=3; i<n; ++i){
+		sum_func=ptrMatrix[i][ite]+ptrMatrix[i-1][ite]+ptrMatrix[i-2][ite]+ptrMatrix[i-3][ite];
+		if(sum_func>maxim){
+			maxim=sum_func;
+		}
+	}
+	if(ite<=18){
+		++ite;
+		rectArriba(ite);
+	}
+}
+
+void rectDerecha(int ite){
+	sum_func=0;
+	for(int j=0; j<17; ++j){
+		sum_func=ptrMatrix[ite][j]+ptrMatrix[ite][j+1]+ptrMatrix[ite][j+2]+ptrMatrix[ite][j+3];
+		if(sum_func>maxim){
+			maxim=sum_func;
+		}
+	}
+	if(ite<=18){
+		++ite;
+		rectDerecha(ite);
+	}
+}
+
+void readBin(){
 	ifstream file;
 	file.open("matriz20x20_float.bin", ios::in | ios::binary);
 	if(file.fail()){
@@ -61,7 +104,7 @@ void readBin(float **ptrMatrix, int n){
 	file.close();
 }
 
-void imp(float **ptrMatrix, int n){
+void imp(){
 	for(int i=0; i<n; ++i){
 		for(int j=0; j<n; ++j){
 			cout<<ptrMatrix[i][j]<<'\t';
